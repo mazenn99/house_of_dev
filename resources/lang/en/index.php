8f34873c933d7a0029981da7e93763378b5478ed
Mazen Alojeel <mazenn99@gmail.com>
2021-02-18 19:40:40 +0300

    first commit

---
+++ resources/lang/en/index.php
@@ -0,0 +1,19 @@
+<?php
+    return [
+        'over_view' => 'We are a Saudi company specialized in technical solutions and artificial intelligence for transport fleets and private and government vehicles, managing a group of ambitious national competencies
+
+We are one of the branches of the Dyarat Trading Company (Closed Joint Stock), a leader in road works, traffic, security, medical and food technologies since 1988.',
+        'licensed_be' => 'The development house is licensed by',
+        'Communication_Technology' => 'Communication and Information Technology Authority',
+        'Public_Transport' => 'Public Transport Authority',
+        'offer_you' => 'What do we offer you',
+        'Tracking_protection_service' => 'Tracking and protection service for private cars',
+        'Tracking_protection_service_overView' => 'A service that enables you to know the location and speed of your car, know the times and places it stops in real time, and provide security services in the event of theft, such as taking a picture of the driver, turning off the engine remotely, and alerting the occurrence of an accident, God forbid, through an application on a smartphone.
+
+We provide our services to you at any authorized service centers spread throughout the Kingdom or send a technical team to serve you at your site',
+        'Operation_card_service' => 'We help you to fulfill the requirements of operating the trucks and extract the operation card in line with the road transport regulations issued by the Public Transport Authority in the fastest time and at the lowest cost',
+        'Integrated_management_systems' => 'Remote tracking and management systems for vehicles with several features through artificial intelligence techniques that enable the user to give orders and view data for the entire fleet through the system with the ability to issue detailed reports according to the user\'s need and add security techniques to prevent thefts or infringement of vehicles, God willing.',
+        'Weights_Matching_Service' => 'We provide truck loading and weighing service to save our customers time and match their actual truck weights with the weight shown in the Transportation Authority systems without the need to increase a truck scale for matching.',
+        'happy_to_serve_you' => 'We are happy to serve you through',
+        'contact_mail' => 'You can contact us via e-mail',
+    ];
